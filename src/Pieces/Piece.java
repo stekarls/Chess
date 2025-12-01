e3a07865e829c3c9bb44b8797e606bac583a7d61
Felix Karlsson <sout.coding@gmail.com>
2025-12-01 13:10:44 +0100

    initial commit

---
+++ src/Pieces/Piece.java
@@ -0,0 +1,14 @@
+package Pieces;
+
+import javax.swing.text.Position;
+
+public abstract class Piece {
+
+    protected String color;
+
+    //Posisjon
+
+
+    public abstract boolean legalMove();
+
+}
