267a7a9589630028be5a0b84224eb39824224f8a
stekarls <sout.coding@gmail.com>
2025-12-03 10:08:44 +0100

    Added support for legalMoves for different colors

--- src/pieces/Pawn.java
+++ src/pieces/Pawn.java
@@ -16,12 +16,9 @@ public class Pawn extends Piece{
 
     @Override
     public boolean legalMove(Position position, ChessBoard board) {
-
         if (!position.legalPosition()){
             return false;
-        }
-
-        if (this.position.equals(position)){
+        }else if (this.position.equals(position)){
             System.out.println("Piece already at same position, try again");
             return false;
         }
@@ -32,42 +29,36 @@ public class Pawn extends Piece{
             legalMoves.add(new Position(this.position.getX() - 1, this.position.getY()));
             legalMoves.add(new Position(this.position.getX() - 1, this.position.getY() - 1));
             legalMoves.add(new Position(this.position.getX() - 1, this.position.getY() + 1));
-
-            if(!this.hasMoved){
-                legalMoves.add(new Position(this.position.getX() - 2, this.position.getY()));
-                
-            }                
-
-            if(!legalMoves.contains(position)){
-                System.out.prinln("Not a legal move, try again");
-                return false;
-            }
-
+            if(!this.hasMoved) legalMoves.add(new Position(this.position.getX() - 2, this.position.getY()));
             
-            if(legalMoves.contains(position)){
-                Piece targetPiece = pieceAtPosition(position);
-                if(targetPiece == null){
-                    this.position = position;
-                    this.hasMoved = true;
-                }else if (targetPiece.getY() == this.position.getY()){ //Check if taregtPiece is on same file
-                    System.out.println("Cannot capture enemy piece in this way");
-                    return false;
-                }
-                else{
-                    
-                }
-            
-            }    
+        }else{
+            legalMoves.add(new Position(this.position.getX() + 1, this.position.getY()));
+            legalMoves.add(new Position(this.position.getX() + 1, this.position.getY() - 1));
+            legalMoves.add(new Position(this.position.getX() + 1, this.position.getY() + 1));
+            if(!this.hasMoved) legalMoves.add(new Position(this.position.getX() + 2, this.position.getY()));
         }
-        
+                       
+        if(!legalMoves.contains(position)){
+            System.out.prinln("Not a legal move, try again");
+            return false;
+        }
 
-
-
-
-
-
-
-
+        if(legalMoves.contains(position)){
+            Piece targetPiece = pieceAtPosition(position);
+            if(targetPiece == null){
+                this.hasMoved = true;
+                return true;
+            }else if(targetPiece.getColor.equals(this.color)){ //Target piece is same color
+                System.out.println("Du har en brikke her fra før");
+                return false;
+            }else if (targetPiece.getY() == this.position.getY()){ //Check if targetPiece is on same file
+                System.out.println("Cannot capture enemy piece in this way");
+                return false;
+            }else{
+                this.hasMoved = true;
+                return true;
+            }
+        }    
     }
 
     public boolean getHasMoved(){
